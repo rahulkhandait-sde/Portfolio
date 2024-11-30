6dea60a8596607d62fc61f02b76e21e58326eb1f
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2024-11-30 23:28:42 +0530

    file updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,8 +34,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
-        </p>
+          Copyright © 2024 Rahul Khandait
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
